52de1ffed9ca5fdc72d4fff70b2e7b07fb9f25f1
Apoorv Sharma <apoorv.kumar.sharma@gmail.com>
2025-01-07 08:03:14 +0530

    feat: server starts configured

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { config } from "./config/index.js"
 
 const app = express()
 
-app.get("/test", (_, res) => { res.send({ "response": "OK" }) })
+app.get("/health", (_, res) => {
+  res.send({ "response": "OK" })
+})
 
 app.listen(config.PORT, () => { console.log("Server started on port 8080") })
